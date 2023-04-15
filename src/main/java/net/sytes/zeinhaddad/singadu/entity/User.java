e91a6df7d6b4021cc3c85b08806a9311bdb532fe
zein3 <zeinhaddad02@gmail.com>
2023-04-16 06:09:23 +0700

    initial account created done

--- src/main/java/net/sytes/zeinhaddad/singadu/entity/User.java
+++ src/main/java/net/sytes/zeinhaddad/singadu/entity/User.java
@@ -2,6 +2,8 @@ package net.sytes.zeinhaddad.singadu.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -13,6 +15,7 @@ import lombok.NoArgsConstructor;
 @Table(name = "users")
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
