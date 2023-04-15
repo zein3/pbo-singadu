2539f6d5f0e08aade4672c43abbbd72ddb192dab
zein3 <zeinhaddad02@gmail.com>
2023-04-15 15:52:08 +0700

    initial commit

---
+++ src/main/java/net/sytes/zeinhaddad/singadu/SingaduApplication.java
@@ -0,0 +1,13 @@
+package net.sytes.zeinhaddad.singadu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingaduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingaduApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/sytes/zeinhaddad/singadu/SingaduApplicationTests.java
@@ -0,0 +1,13 @@
+package net.sytes.zeinhaddad.singadu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingaduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
