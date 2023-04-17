0d85eff4c13263722ae4fda41e025dbb9ac3a57f
zein3 <zeinhaddad02@gmail.com>
2023-04-17 11:16:29 +0700

    crud selesai

--- src/main/java/net/sytes/zeinhaddad/singadu/controller/UserController.java
+++ src/main/java/net/sytes/zeinhaddad/singadu/controller/UserController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,6 +49,11 @@ public class UserController {
         user.setRole(role);
 
         return this.userService.saveUser(user);
+    }
+
+    @PutMapping
+    public Long update(@RequestBody User user) {
+        return this.userService.updateUser(user);
     }
 
     @DeleteMapping("/{userId}")
