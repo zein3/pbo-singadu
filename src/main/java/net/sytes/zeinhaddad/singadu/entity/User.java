c21f1322c39635400e7c1b4db30d81bca7a6b805
zein3 <zeinhaddad02@gmail.com>
2023-05-02 14:04:29 +0700

    minor changes

--- src/main/java/net/sytes/zeinhaddad/singadu/entity/User.java
+++ src/main/java/net/sytes/zeinhaddad/singadu/entity/User.java
@@ -13,7 +13,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
